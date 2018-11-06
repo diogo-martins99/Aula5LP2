ee7b1efe4b8e7bda6d71e68f1ba0eddba4c1b025
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-11-06 10:22:05 +0000

    Add try catch with exception

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -11,8 +11,17 @@ namespace ExceptionTest
         static void Main(string[] args)
         {
             Console.Write("Insere um numero inteiro: ");
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero inserido: {i} ");
+
+            try
+            {
+               int i = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine($"Numero inserido: {i} ");
+            } catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu o seguinte erro: " +
+                    e.Message);
+            }
+
         }
     }
 }
