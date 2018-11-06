ddf1cadaf65d6501d59808db8a35b6e0c70049cb
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-11-06 10:09:42 +0000

    Add simple 'Insert Number' request

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -10,6 +10,9 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
+            Console.Write("Insere um numero inteiro: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero inserido: {i} ");
         }
     }
 }
